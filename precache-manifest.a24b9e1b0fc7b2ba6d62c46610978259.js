a0a6ec074278b1df4769f8b7b06a79192c359e55
OlegBulgakovWork <obulgakovwork@gmail.com>
2020-07-15 19:03:20 +0300

    Updates

---
+++ precache-manifest.a24b9e1b0fc7b2ba6d62c46610978259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e648747a3fb52669df523f62f6738140",
+    "url": "/TestReact/index.html"
+  },
+  {
+    "revision": "bb0e12aa0bd8201262b4",
+    "url": "/TestReact/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "f4569c1843ac8bf6d9b3",
+    "url": "/TestReact/static/css/main.b6404239.chunk.css"
+  },
+  {
+    "revision": "bb0e12aa0bd8201262b4",
+    "url": "/TestReact/static/js/2.91c2f579.chunk.js"
+  },
+  {
+    "revision": "4aaf2b8666524732757dc85ab41be7dd",
+    "url": "/TestReact/static/js/2.91c2f579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4569c1843ac8bf6d9b3",
+    "url": "/TestReact/static/js/main.7e1ce4d1.chunk.js"
+  },
+  {
+    "revision": "2422882badfeb48e03ca",
+    "url": "/TestReact/static/js/runtime-main.ee4b089a.js"
+  }
+]);
